8555df66bd9209cfa8c8334baa91d64c79cc4e09
Gage Vander Clay <gvanderclay@gmail.com>
2020-11-27 15:55:09 -0500

    Remove postgres type

--- src/entities-and-migrations.ts
+++ src/entities-and-migrations.ts
@@ -1,42 +1,34 @@
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { ConnectionOptions } from "typeorm";
 
 export type EntitiesAndMigrationsOpts = Pick<
-  PostgresConnectionOptions,
+  ConnectionOptions,
   "entities" | "migrations"
 >;
-const requireEntities = require.context("./entity/", true, /\.ts$/);
-const entitiesViaWebpack = requireEntities
-  .keys()
-  .sort()
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  .map((filename) => {
-    const required = requireEntities(filename);
-    return Object.keys(required).reduce((result, exportedKey) => {
-      const exported = required[exportedKey];
-      if (typeof exported === "function") {
-        return result.concat(exported);
-      }
-      return result;
-    }, [] as NonNullable<EntitiesAndMigrationsOpts["entities"]>);
-  })
-  .flat();
 
-const requireMigrations = require.context("./migration/", true, /\.ts$/);
-const migrationsViaWebpack = requireMigrations
-  .keys()
-  .sort()
-  .map((filename) => {
-    // Allows for export of non default migrations
-    const required = requireMigrations(filename);
-    return Object.keys(required).reduce((result, exportedKey) => {
-      const exported = required[exportedKey];
-      if (typeof exported === "function") {
-        return result.concat(exported);
-      }
-      return result;
-    }, [] as NonNullable<EntitiesAndMigrationsOpts["migrations"]>);
-  })
-  .flat();
+const importAllFunctions = (
+  requireContext: __WebpackModuleApi.RequireContext
+) =>
+  requireContext
+    .keys()
+    .sort()
+    .map((filename) => {
+      const required = requireContext(filename);
+      return Object.keys(required).reduce((result, exportedKey) => {
+        const exported = required[exportedKey];
+        if (typeof exported === "function") {
+          return result.concat(exported);
+        }
+        return result;
+      }, [] as any);
+    })
+    .flat();
+
+const entitiesViaWebpack: NonNullable<
+  EntitiesAndMigrationsOpts["entities"]
+> = importAllFunctions(require.context("./entity/", true, /\.ts$/));
+const migrationsViaWebpack: NonNullable<
+  EntitiesAndMigrationsOpts["migrations"]
+> = importAllFunctions(require.context("./migration/", true, /\.ts$/));
 
 export const entitiesAndMigrations: EntitiesAndMigrationsOpts = {
   entities: entitiesViaWebpack,
